d61fe75bd37c86e64c54ece2a5440392bf9fbf9c
Furkan Aydemir <sem_203@hotmail.com>
2026-03-24 18:28:34 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // İstersen QR Code tasarımındaki özel renkleri buraya ekleyebilirsin
         'slate-300': '#D5E1EF',
         'slate-500': '#68778D',
         'slate-900': '#1F314F',
